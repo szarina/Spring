e6b0fbc768e8d190d9ec378aca7adf951f9ed026
szarina <zarina.saden@gmail.com>
2024-02-25 17:41:56 +0600

    Finished task1
    greeting controller + service

---
+++ week1/src/main/java/com/example/week1/сontroller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.example.week1.сontroller;
+
+import com.example.week1.model.Greeting;
+import com.example.week1.service.GreetingService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+public class GreetingController {
+
+    GreetingService greetingService = new GreetingService();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return greetingService.generateGreeting(name);
+    }
+}
