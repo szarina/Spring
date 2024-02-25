6c51a06c623ce755c4582f0baf0697377adb3fbc
szarina <zarina.saden@gmail.com>
2024-02-25 17:46:18 +0600

    Created task 2 of week1

---
+++ week1task2/src/main/java/com/example/week1task2/Week1task2Application.java
@@ -0,0 +1,13 @@
+package com.example.week1task2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1task2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1task2Application.class, args);
+	}
+
+}
---
+++ week1task2/src/test/java/com/example/week1task2/Week1task2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.week1task2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1task2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
