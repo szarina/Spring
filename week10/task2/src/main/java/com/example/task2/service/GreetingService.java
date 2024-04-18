5b3c5cf228cfec394aacb16fbcd106cf0d838eeb
szarina <zarina.saden@gmail.com>
2024-04-18 20:55:56 +0500

    Add heroku.yml

---
+++ week10/task2/src/main/java/com/example/task2/service/GreetingService.java
@@ -0,0 +1,16 @@
+package com.example.week1.service;
+
+import com.example.week1.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GreetingService {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public Greeting generateGreeting(String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
